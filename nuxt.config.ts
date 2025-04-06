9649cf416a98caeedf99eb83ffafd0b25b6809d0
facuagueria <fagueria@sugit.com.ar>
2025-04-06 19:23:51 -0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+      },
+    },
+  },
+});
