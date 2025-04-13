bb3a4fa83bfb525498c8eef944b1d8dbd9d2cc28
facuagueria <fagueria@sugit.com.ar>
2025-04-12 21:44:00 -0300

    feat(ai): create service for generating texts

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,3 +1,5 @@
+import { createOllamaModel, createOpenAIModel, generateChatResponse } from '~~/server/services/ai-service';
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { messages } = body;
@@ -5,9 +7,18 @@ export default defineEventHandler(async (event) => {
   const id = messages.length.toString();
   const lastMessage = messages[messages.length - 1];
 
+  const openaiApiKey = useRuntimeConfig().openaiApiKey;
+  const openaiModel = createOpenAIModel(openaiApiKey);
+  const ollamaModel = createOllamaModel();
+
+  const response = await generateChatResponse(
+    openaiModel,
+    messages,
+  );
+
   return {
     id,
     role: 'assistant',
-    content: `(server) You said: ${lastMessage.content}`,
+    content: response,
   };
 });
