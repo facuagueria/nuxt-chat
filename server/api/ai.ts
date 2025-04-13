6794062c94851b85b962c8b9f6a73517dd5a43f0
facuagueria <fagueria@sugit.com.ar>
2025-04-12 23:04:37 -0300

    chore: clean up

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,15 +1,19 @@
-import { createOllamaModel, createOpenAIModel, generateChatResponse } from '~~/server/services/ai-service';
+import {
+  // createOllamaModel,
+  createOpenAIModel,
+  generateChatResponse,
+} from '~~/server/services/ai-service';
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { messages } = body;
 
   const id = messages.length.toString();
-  const lastMessage = messages[messages.length - 1];
 
   const openaiApiKey = useRuntimeConfig().openaiApiKey;
   const openaiModel = createOpenAIModel(openaiApiKey);
-  const ollamaModel = createOllamaModel();
+
+  // const ollamaModel = createOllamaModel();
 
   const response = await generateChatResponse(
     openaiModel,
