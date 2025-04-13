ffdb0fd3b40bfe99d9b19e618a6415ace2f0bd0a
facuagueria <fagueria@sugit.com.ar>
2025-04-12 21:44:19 -0300

    feat(app): app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/eslint'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    openaiApiKey: '',
+  },
   future: {
     compatibilityVersion: 4,
   },
