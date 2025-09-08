efb717bdfe4dbaa0ce57f5f12b5aaa1dde8de8d8
facuagueria <fagueria@sugit.com.ar>
2025-09-08 19:42:39 -0300

    feat(chats): caching chat response

--- layers/chat/server/api/chats/index.get.ts
+++ layers/chat/server/api/chats/index.get.ts
@@ -1,6 +1,20 @@
 import type { H3Event } from 'h3';
 import { getAllChats } from '#layers/chat/server/repository/chatRepository';
 
-export default defineEventHandler(async (_event: H3Event) => {
+export default defineCachedEventHandler(async (_event: H3Event) => {
+  const storage = useStorage('db');
+  await storage.setItem('chats:has-new-chat', false);
+
   return await getAllChats();
+}, {
+  name: 'getAllChats',
+  maxAge: 0,
+  swr: false,
+  async shouldInvalidateCache() {
+    const storage = useStorage('db');
+
+    const hasNewChat = await storage.getItem<boolean>('chats:has-new-chat');
+
+    return hasNewChat || false;
+  },
 });
--- layers/chat/server/api/chats/index.post.ts
+++ layers/chat/server/api/chats/index.post.ts
@@ -4,6 +4,10 @@ import { createChat } from '#layers/chat/server/repository/chatRepository';
 export default defineEventHandler(async (event: H3Event) => {
   const { title, projectId } = await readBody(event);
 
+  const storage = useStorage('db');
+
+  await storage.setItem('chats:has-new-chat', true);
+
   return await createChat({
     title,
     projectId,
